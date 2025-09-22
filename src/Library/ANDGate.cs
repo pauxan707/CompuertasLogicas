41bc9a40ff23b8f4a91ae5131a6745319fcda905
Pau <pauxxann@gmail.com>
2025-09-22 12:06:36 -0300

    ihygutrdhfhyj

---
+++ src/Library/ANDGate.cs
@@ -0,0 +1,30 @@
+﻿namespace Library;
+
+public class ANDGate: IGate
+{
+    public string Name { get; set; }
+    public List<IGate> Input = new List<IGate>();
+    public IGate Output { get; set; }
+
+    public ANDGate(string name)
+    {
+        Name = name;
+    }
+
+    public void AddInput(IGate iGate)
+    {
+        Input.Add(iGate);
+    }
+
+    public IGate Evaluate()
+    {
+        foreach (IGate element in this.Input)
+        {
+            if (element == false)
+            {
+                
+            }
+        }
+        
+    }
+}
